0637774e4ee9f7c658ed28d1475dcea0ec27fd83
Jahangir Kazimzada <ckazimzada02@gmail.com>
2025-12-14 19:01:33 +0400

    updated

--- index.php
+++ index.php
@@ -15,8 +15,7 @@
         $array[] = $element;
 
 
-        $length = count($array);
-        for ($i=0; $i < $length; $i++) { 
+        for ($i=0; $i < count($array); $i++) { 
             echo $array[$i] , "<br/>";
         }
     }
